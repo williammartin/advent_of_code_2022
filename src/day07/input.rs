be28c810257b2794c5f989e477952a5f990d7fb6
William Martin <williammartin@github.com>
2022-12-07 21:36:16 +0100

    Day 7 borked

--- src/day07/input.rs
+++ src/day07/input.rs
@@ -1,7 +1,25 @@
 use crate::day07::Input;
 
+use super::{File, Line};
+
 const INPUT: &str = include_str!("../../input/07/input.txt");
 
 pub fn read() -> Input {
-    unimplemented!()
+    // let root = File::Directory {
+    //     name: "/".to_owned(),
+    //     files: vec![],
+    // };
+    // let mut cd = root;
+
+    let lines: Vec<Line> = INPUT.lines().map(parse_line).collect();
+    dbg!(lines);
+
+    for line in INPUT.lines() {}
+
+    todo!();
+}
+
+fn parse_line(s: &str) -> Line {
+    // could flat_map at the call site but I guess I'd rather it blew up here
+    s.parse().expect("to parse lines correctly")
 }
--- src/day07/mod.rs
+++ src/day07/mod.rs
@@ -2,9 +2,139 @@ pub mod input;
 pub mod part1;
 pub mod part2;
 
+use std::{
+    borrow::BorrowMut,
+    collections::{hash_map::Entry, HashMap},
+    rc::Rc,
+    str::FromStr,
+};
+
 use crate::{Output, Part};
 
-pub type Input = u8;
+#[derive(Debug)]
+pub enum Command {
+    ChangeDirectory { to: String },
+    List,
+}
+
+#[derive(Debug)]
+pub struct File {
+    name: String,
+    size: u32,
+}
+
+#[derive(Debug)]
+pub enum Line {
+    Command(Command),
+    Directory(String),
+    File(File),
+}
+
+impl FromStr for Line {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Some(command) = s.strip_prefix("$ ") {
+            if let Some(dir) = command.strip_prefix("cd ") {
+                Ok(Line::Command(Command::ChangeDirectory {
+                    to: dir.to_owned(),
+                }))
+            } else {
+                Ok(Line::Command(Command::List))
+            }
+        } else if let Some(dir) = s.strip_prefix("dir ") {
+            Ok(Line::Directory(dir.to_owned()))
+        } else {
+            let (size, name) = s
+                .split_once(" ")
+                .ok_or("failed to parse line, expected a file")?;
+            let size = size
+                .parse::<u32>()
+                .map_err(|_| "failed to parse file size")?;
+
+            Ok(Line::File(File {
+                size,
+                name: name.to_owned(),
+            }))
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Directory {
+    parent: Option<Rc<Directory>>,
+    directories: HashMap<String, Directory>,
+    files: Vec<File>,
+}
+
+impl Directory {
+    fn new(name: &str, parent: Option<Rc<Directory>>) -> Directory {
+        Directory {
+            parent,
+            directories: HashMap::new(),
+            files: Vec::new(),
+        }
+    }
+
+    fn add_dir(&mut self, name: &str) -> &mut Directory {
+        let new_dir = Directory::new(name, Some(Rc::new(*self)));
+        self.directories.entry(name.to_owned()).or_insert(new_dir)
+    }
+
+    fn add_file(&mut self, name: String, size: u32) {
+        self.files.push(File {
+            name: name.to_owned(),
+            size,
+        })
+    }
+}
+
+impl FromIterator<Line> for Directory {
+    fn from_iter<T: IntoIterator<Item = Line>>(iter: T) -> Self {
+        let mut root = Directory::new("/", None);
+        let mut pwd = Rc::new(&mut root);
+
+        for i in iter {
+            match i {
+                Line::Command(cmd) => match cmd {
+                    Command::ChangeDirectory { to } => {
+                        if to == "/" {
+                            pwd = Rc::new(&mut root);
+                        }
+
+                        if to == ".." {
+                            pwd = pwd.parent.expect("to have a parent").borrow_mut();
+                            // pwd_stack.pop(); // ignore popped dir
+                        }
+
+                        let new_dir = pwd_stack
+                            .last_mut()
+                            .expect("to always have a pwd")
+                            .add_dir(to.as_str());
+                        pwd_stack.push(new_dir);
+                    }
+                    Command::List => continue,
+                },
+                Line::Directory(name) => {
+                    pwd_stack
+                        .last_mut()
+                        .expect("to always have a pwd")
+                        .add_dir(name.as_str()); // no need to push onto pwd_stack, just noting there is a dir
+                }
+                Line::File(file) => {
+                    pwd_stack
+                        .last_mut()
+                        .expect("to always have a pwd")
+                        .add_file(file.name, file.size);
+                }
+            }
+        }
+
+        root
+    }
+}
+
+pub type Input = Directory;
 
 pub fn run(part: Part) -> Output {
     let input = input::read();
